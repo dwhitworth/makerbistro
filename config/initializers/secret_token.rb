f0bd3365ec70f4ddf0ca55a53d57efa97e50b13c
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-03-20 09:36:54 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MakerBistro::Application.config.secret_key_base = 'e2e7b6daff93bab27ce6e2f80e308f2e04bb787f04f294a015ea4c4deb998c3759517b82820bed137ea1d978f0b6fa701e2bc57e313be91c2366ab62227e0b0a'
