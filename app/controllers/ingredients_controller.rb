15a4de5886e8e982dc8f1a42f8e719433866c3f4
Neil Mithipati <neil.mithipati@gmail.com>
2014-03-20 10:31:36 -0500

    Instantiate resources

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,19 +1,31 @@
 class IngredientsController < ApplicationController
   def new
+    @ingredient = Ingredient.new
   end
 
   def create
+    @ingredient = Ingredient.create(ingredient_params)
+
+    if @ingredient.save
+    else
+    end
   end
 
   def edit
+    @ingredient = Ingredient.find(params[:id])
   end
 
   def update
+    @ingredient = Ingredient.update_attributes(ingredient_params)
   end
 
   def show
+    @ingredient = Ingredient.find(params[:id])
   end
 
   def destroy
+    @ingredient = Ingredient.find(params[:id])
+
+    @ingredient.destroy
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,23 +1,41 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.create(item_params)
+
+    if @item.save
+    else
+    end
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.update_attributes(item_params)
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def destroy
+    @item = Item.find(params[:id])
+
+    @item.destroy
   end
+
+  private
+      def item_params
+        params.require(:item).permit(:name, :price)
+      end
 
 end
